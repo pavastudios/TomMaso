1b24612d0488255dcfd1a3f25c42eee6402cd4ec
pCosta01 <dicostanzopasquale01@gmail.com>
2021-05-17 18:40:31 +0200

    I CDN VINCONO, implementato il parser del md

--- src/main/java/com/pavastudios/TomMaso/servlets/ViewerServlet.java
+++ src/main/java/com/pavastudios/TomMaso/servlets/ViewerServlet.java
@@ -5,6 +5,8 @@ import com.pavastudios.TomMaso.servlets.MasterServlet;
 import com.pavastudios.TomMaso.utility.*;
 import com.pavastudios.TomMaso.utility.Session;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,15 +17,23 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.SQLException;
 
+import static com.pavastudios.TomMaso.utility.FileUtility.getFileType;
+
 @WebServlet(name = "NewViewer", urlPatterns = {"/blogs/*","/users/*"})
 public class ViewerServlet extends MasterServlet {
-    private void manageFile(HttpServletRequest req, HttpServletResponse resp, File file) throws IOException {
+    private void manageFile(HttpServletRequest req, HttpServletResponse resp, File file) throws IOException, ServletException {
+        if(getFileType(getServletContext(), file)== FileUtility.FileType.MARKDOWN){
+            req.setAttribute("file",file);
+            getServletContext().getRequestDispatcher("/WEB-INF/jsp/bootstrap/markdownViewer.jsp").forward(req,resp);
+            return ;
+        }
         OutputStream out = resp.getOutputStream();
         FileInputStream fr = new FileInputStream(file);
         resp.setContentType(getServletContext().getMimeType(file.getAbsolutePath()));
         //resp.setHeader("Content-Disposition", "attachment;filename="+file.getName());
         FileUtility.writeFile(fr, out);
     }
+
     @Override
     protected void doGet(Session session, HttpServletRequest req, HttpServletResponse resp) throws SQLException, ServletException, IOException {
         File file=null;
---
+++ src/main/webapp/WEB-INF/jsp/bootstrap/markdownViewer.jsp
@@ -0,0 +1,38 @@
+<%@ page import="java.io.File" %>
+<%@ page import="java.io.FileInputStream" %><%--
+  Created by IntelliJ IDEA.
+  User: pasqu
+  Date: 17/05/2021
+  Time: 17.33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <%@include file="general/headTags.jsp"%>
+    <%
+        Utente user = ses.getUtente();
+        File file = (File) request.getAttribute("file");
+        FileInputStream fis = new FileInputStream(file);
+        StringBuilder content = new StringBuilder();
+        int readChar;
+        while((readChar = fis.read()) != -1) {
+            content.append((char)readChar);
+        }
+    %>
+
+    <title><%=file.getName()%></title>
+</head>
+<body>
+<%@include file="general/navbar.jsp"%>
+<div id="content">
+</div>
+<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+<%@include file="general/footer.jsp"%>
+<!--<%@include file="general/tailTag.jsp"%>!-->
+
+<script>
+    document.getElementById('content').innerHTML = marked("<%=content.toString().replace("\r\n","\\\n").replace("\"","\\\"")%>");
+</script>
+</body>
+</html>
