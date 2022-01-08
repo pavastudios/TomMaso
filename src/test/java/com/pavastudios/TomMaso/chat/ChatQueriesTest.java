66c32892b4a5ea0f18cb900deb41bde2a69d839c
Tonin-ai <a.allocca18@studenti.unisa.it>
2022-01-08 17:09:10 +0100

    aggiustato testing chat

--- src/test/java/com/pavastudios/TomMaso/chat/ChatQueriesTest.java
+++ src/test/java/com/pavastudios/TomMaso/chat/ChatQueriesTest.java
@@ -20,71 +20,76 @@ class ChatQueriesTest extends TestDBConnection {
 
     public static Stream<Arguments> testSendMessaggio() throws SQLException {
         return Stream.of(
-                Arguments.of(null, null, null, false),
-                Arguments.of(ChatQueries.findChatById(10), UserQueries.findUserById(3), "Chat_testing_is_awesome!", false),
-                Arguments.of(ChatQueries.findChatById(10), UserQueries.findUserById(3), null, false),
-                Arguments.of(ChatQueries.findChatById(10), null, "Chat_testing_is_awesome!", false),
-                Arguments.of(null, UserQueries.findUserById(3), "Chat_testing_is_awesome!", false),
-                Arguments.of(ChatQueries.findChatById(10), UserQueries.findUserById(1), "", false),
-                Arguments.of(ChatQueries.findChatById(10), UserQueries.findUserById(1), "Chat_testing_is_awesome!", true)
+                Arguments.of(0, 0, null, false),
+                Arguments.of(10, 3, "Chat_testing_is_awesome!", false),
+                Arguments.of(10, 3, null, false),
+                Arguments.of(10, 0, "Chat_testing_is_awesome!", false),
+                Arguments.of(0, 3, "Chat_testing_is_awesome!", false),
+                Arguments.of(10, 1, "", false),
+                Arguments.of(10, 1, "Chat_testing_is_awesome!", true)
         );
     }
 
     public static Stream<Arguments> testCreateChat() throws SQLException {
         return Stream.of(
-                Arguments.of(null, null, false),
-                Arguments.of(UserQueries.findUserById(1), null, false),
-                Arguments.of(null, UserQueries.findUserById(2), false),
-                Arguments.of(UserQueries.findUserById(1), UserQueries.findUserById(3), true) //Non si possono creare chat che già esistono (errore con il database)
+                Arguments.of(0, 0, false),
+                Arguments.of(1, 0, false),
+                Arguments.of(0, 2, false),
+                Arguments.of(1, 3, true) //Non si possono creare chat che già esistono (errore con il database)
         );
     }
 
     public static Stream<Arguments> testFindUserChats() throws SQLException {
         return Stream.of(
-                Arguments.of(UserQueries.findUserById(1), true),
-                Arguments.of(UserQueries.findUserById(4), false)
+                Arguments.of(1, true),
+                Arguments.of(4, false)
         );
     }
 
     public static Stream<Arguments> testFindChatByUsersTest() throws SQLException {
         return Stream.of(
-                Arguments.of(null, null, false),
-                Arguments.of(UserQueries.findUserById(1), null, false),
-                Arguments.of(null, UserQueries.findUserById(2), false),
-                Arguments.of(UserQueries.findUserById(1), UserQueries.findUserById(2), true)
+                Arguments.of(0, 0, false),
+                Arguments.of(1, 0, false),
+                Arguments.of(0, 2, false),
+                Arguments.of(1, 2, true)
         );
     }
 
     public static Stream<Arguments> testFetchMessageFromId() throws SQLException {
         return Stream.of(
-                Arguments.of(ChatQueries.findChatById(10), 8, false),
-                Arguments.of(ChatQueries.findChatById(10), 9, true)
+                Arguments.of(10, 8, false),
+                Arguments.of(10, 9, true)
         );
     }
 
     public static Stream<Arguments> testDeleteMessaggio() throws SQLException {
         return Stream.of(
-                Arguments.of(ChatQueries.findMessageById(9))
+                Arguments.of(9)
         );
     }
 
     @ParameterizedTest
     @MethodSource("testSendMessaggio")
-    public void sendTextToChatTest(Chat chat, Utente mittente, String testo, boolean flag) throws SQLException {
+    public void sendTextToChatTest(int idchat, int idmittente, String testo, boolean flag) throws SQLException {
+        Chat chat=ChatQueries.findChatById(idchat);
+        Utente mittente=UserQueries.findUserById(idmittente);
         if (flag) assertNotNull(ChatQueries.sendTextToChat(chat, mittente, testo));
         else assertNull(ChatQueries.sendTextToChat(chat, mittente, testo));
     }
 
     @ParameterizedTest
     @MethodSource("testCreateChat")
-    public void createChatTest(Utente u1, Utente u2, boolean flag) throws SQLException {
+    public void createChatTest(int id1, int id2, boolean flag) throws SQLException {
+        Utente u1=UserQueries.findUserById(id1);
+        Utente u2=UserQueries.findUserById(id2);
         if (flag) assertNotNull(ChatQueries.createChat(u1, u2));
         else assertNull(ChatQueries.createChat(u1, u2));
     }
 
     @ParameterizedTest
     @MethodSource("testFindUserChats")
-    public void findUserChatsTest(Utente user, boolean flag) throws SQLException {
+    public void findUserChatsTest(int id, boolean flag) throws SQLException {
+        Utente user=UserQueries.findUserById(id);
         if (flag) assertNotNull(ChatQueries.findUserChats(user));
         else {
             if (ChatQueries.findUserChats(user).isEmpty()) assertTrue(true);
@@ -93,14 +98,17 @@ class ChatQueriesTest extends TestDBConnection {
 
     @ParameterizedTest
     @MethodSource("testFindChatByUsersTest")
-    public void findChatByUsersTest(@Nullable Utente u1, @Nullable Utente u2, boolean flag) throws SQLException {
+    public void findChatByUsersTest(int id1, int id2, boolean flag) throws SQLException {
+        Utente u1=UserQueries.findUserById(id1);
+        Utente u2=UserQueries.findUserById(id2);
         if (flag) assertNotNull(ChatQueries.findChatByUsers(u1, u2));
         else assertNull(ChatQueries.findChatByUsers(u1, u2));
     }
 
     @ParameterizedTest
     @MethodSource("testFetchMessageFromId")
-    public void fetchMessageFromIdTest(Chat chat, int fromId, boolean flag) throws SQLException {
+    public void fetchMessageFromIdTest(int id, int fromId, boolean flag) throws SQLException {
+       Chat chat = ChatQueries.findChatById(id);
         if (flag) assertNotNull(ChatQueries.fetchMessageFromId(chat, fromId));
         else {
             if (ChatQueries.fetchMessageFromId(chat, fromId).isEmpty()) assertTrue(true);
@@ -109,8 +117,8 @@ class ChatQueriesTest extends TestDBConnection {
 
     @ParameterizedTest
     @MethodSource("testDeleteMessaggio")
-    public void deleteMessageTest(Messaggio messaggio) throws SQLException {
-        ChatQueries.deleteMessage(messaggio);
+    public void deleteMessageTest(int id) throws SQLException {
+        ChatQueries.deleteMessage(ChatQueries.findMessageById(id));
         assertNull(ChatQueries.findMessageById(9));
     }
 
